f40a2463b54cdb45a21a512c2370db8d28e070db
Daniel <daniel.henriques.gamedev@gmail.com>
2026-03-17 11:48:45 +0000

    Add class doc

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -2,6 +2,9 @@
 
 namespace LevelOne
 {
+    /// <summary>
+    /// This is the main file of this program.
+    /// </summary>
     public class MainIsHere
     {
         private static void Main(string[] args)
