a7a8a9c0be15647f689231afbc7a496120212ab0
Daniel <daniel.henriques.gamedev@gmail.com>
2026-03-17 11:25:24 +0000

    Add initial code

---
+++ LevelOne/MainIsHere.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LevelOne
+{
+    public class MainIsHere
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
