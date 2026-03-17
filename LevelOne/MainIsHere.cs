3d6ebdd7e78922a92c6dc149cde5b239959aa25e
Daniel <daniel.henriques.gamedev@gmail.com>
2026-03-17 11:52:38 +0000

    Add method doc

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -4,6 +4,10 @@ namespace LevelOne
 {
     public class MainIsHere
     {
+        /// <summary>
+        /// The program starts here
+        /// </summary>
+        /// <param name="args">the command line arguments are here</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
