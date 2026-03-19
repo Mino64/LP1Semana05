356f4de729fb1dc630aeafc10d43efc329a35e39
Daniel <daniel.henriques.gamedev@gmail.com>
2026-03-19 16:22:54 +0000

    add code for corelogic

--- DungeonStats/Program.cs
+++ DungeonStats/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace DungeonStats
 {
     public class Program
     {
+       
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            int savedDef = Convert.ToInt32(args[1]);
         }
-    }
-}
+
+        private static int Damage(int attack, int defense)
+        {
+    
+            if((attack - defense) < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return attack - defense;
+            }
+        }
+        private static int Damage(int attack)
+        {
+                return attack;
+            }
+
+        private static int CriticalHit(int damage)
+        {
+            if(damage <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1 + CriticalHit(Damage(damage, damage));
+            }
+        }
+
+        }
+
+    } 
